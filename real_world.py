437f696839787ae15488debd8b8b8c7ec9523faf
Robert Schneider <robmschn@gmail.com>
2019-11-26 14:38:17 -0500

    fast way enabled

--- real_world.py
+++ real_world.py
@@ -23,25 +23,26 @@ img_urls = [
 t1 = time.perf_counter()
 
 
-# def download_image(img_url):
-#     img_bytes = requests.get(img_url).content
-#     img_name = img_url.split('/')[3]
-#     img_name = f'{img_name}.jpg'
-#     with open(img_name, 'wb') as img_file:
-#         img_file.write(img_bytes)
-#         print(f'{img_name} was downloaded...')
-
-
-# with concurrent.futures.ThreadPoolExecutor() as executor:
-#     executor.map(download_image, img_urls)
-
-for img_url in img_urls:
+def download_image(img_url):
     img_bytes = requests.get(img_url).content
     img_name = img_url.split('/')[3]
     img_name = f'{img_name}.jpg'
     with open(img_name, 'wb') as img_file:
         img_file.write(img_bytes)
-        print (f'{img_name} was downloaded...')
+        print(f'{img_name} was downloaded...')
+
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(download_image, img_urls)
+
+# slow way
+# for img_url in img_urls:
+#     img_bytes = requests.get(img_url).content
+#     img_name = img_url.split('/')[3]
+#     img_name = f'{img_name}.jpg'
+#     with open(img_name, 'wb') as img_file:
+#         img_file.write(img_bytes)
+#         print (f'{img_name} was downloaded...')
 
 t2 = time.perf_counter()
 
