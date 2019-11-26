7e2f96b6e0c347a01669f499fda9c719793cfac9
Robert Schneider <robmschn@gmail.com>
2019-11-26 13:37:35 -0500

    added join so threads finish before finish time

--- learn_thread.py
+++ learn_thread.py
@@ -14,6 +14,9 @@ t2 = threading.Thread(target=do_something)
 t1.start()
 t2.start()
 
+t1.join()
+t2.join()
+
 finish = time.perf_counter()
 
 print (f'Finished in {round(finish-start, 2)} second(s)')
