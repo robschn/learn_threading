a43146ef7d0114d7fad72c4d62798d35c45a6f75
Robert Schneider <robmschn@gmail.com>
2019-11-26 14:03:36 -0500

    used map to return result in order

--- new_thread.py
+++ new_thread.py
@@ -10,10 +10,10 @@ def do_something(seconds):
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
     secs = [5, 4, 3, 2, 1]
-    results = [executor.submit(do_something, sec) for sec in secs]
+    results = executor.map(do_something, secs)
 
-    for f in concurrent.futures.as_completed(results):
-        print (f.result())
+    for result in results:
+        print (result)
 
 finish = time.perf_counter()
 
