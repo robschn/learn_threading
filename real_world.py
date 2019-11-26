ba40e18a615fb5ce55422e13f54e88b805b09924
Robert Schneider <robmschn@gmail.com>
2019-11-26 14:11:56 -0500

    added slower way and commented faster one

--- real_world.py
+++ real_world.py
@@ -23,18 +23,25 @@ img_urls = [
 t1 = time.perf_counter()
 
 
-def download_image(img_url):
+# def download_image(img_url):
+#     img_bytes = requests.get(img_url).content
+#     img_name = img_url.split('/')[3]
+#     img_name = f'{img_name}.jpg'
+#     with open(img_name, 'wb') as img_file:
+#         img_file.write(img_bytes)
+#         print(f'{img_name} was downloaded...')
+
+
+# with concurrent.futures.ThreadPoolExecutor() as executor:
+#     executor.map(download_image, img_urls)
+
+for img_url in img_urls:
     img_bytes = requests.get(img_url).content
     img_name = img_url.split('/')[3]
     img_name = f'{img_name}.jpg'
     with open(img_name, 'wb') as img_file:
         img_file.write(img_bytes)
-        print(f'{img_name} was downloaded...')
-
-
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    executor.map(download_image, img_urls)
-
+        print (f'{img_name} was downloaded...')
 
 t2 = time.perf_counter()
 
