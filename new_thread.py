0cc55ad42a73ebc614fd2900467aa9eac9a9b334
Robert Schneider <robmschn@gmail.com>
2019-11-26 14:00:23 -0500

    sunmit runs for each sec results in order

--- new_thread.py
+++ new_thread.py
@@ -6,13 +6,14 @@ start = time.perf_counter()
 def do_something(seconds):
     print (f'Sleeping {seconds} second(s)...')
     time.sleep(seconds)
-    return 'Done sleeping.'
+    return f'Done sleeping.{seconds}'
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    f1 = executor.submit(do_something, 1)
-    f2 = executor.submit(do_something, 1)
-    print (f1.result())
-    print (f1.result())
+    secs = [5, 4, 3, 2, 1]
+    results = [executor.submit(do_something, sec) for sec in secs]
+
+    for f in concurrent.futures.as_completed(results):
+        print (f.result())
 
 finish = time.perf_counter()
 
